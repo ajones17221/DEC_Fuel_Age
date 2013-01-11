7ad0a6264c944e45143d4e444a9f2a0faaba7e35
Andrew Dennison <andrew.s.dennison@gmail.com>
2013-01-11 16:29:59 +0800

    Added new.year = old.year case

--- Create_Trigger.sql
+++ Create_Trigger.sql
@@ -1,8 +1,7 @@
 CREATE OR REPLACE FUNCTION calc_fuel_age() RETURNS trigger as $$
 DECLARE
-	cur_rec 	RECORD; --cursor variable
-	--fire_history 	-- name of fire history table
-	--fuel_age 	--name of fuel load table
+	cur_rec 	RECORD;		--cursor variable
+	cur_geom 	VARCHAR; 	--current geometry variable
 BEGIN
 	IF (TG_OP = 'INSERT') THEN
 		-- Check for intersecting polygons, if there aren't any just insert the record into fuel_history
@@ -13,18 +12,47 @@ BEGIN
 			FOR cur_rec IN SELECT * FROM fuel_age_test AS a JOIN dec_fire_history_test AS b ON ST_Intersects(a.geom, b.geom) WHERE b.gid = new.gid ORDER BY a.year1 LOOP
 				--If the inserted record is older than the intersecting record update the geometry of the intersecting record
 				IF new.year1 > cur_rec.year1 THEN
+					--Get the geometry of the current record (new geometry - current geometry)
+					cur_geom := (SELECT ST_AsText(ST_MULTI(ST_DIFFERENCE(a.geom,b.geom)))
+						FROM fuel_age_test AS a, dec_fire_history_test AS b
+						WHERE a.gid = cur_rec.gid and b.gid = new.gid);
+
+					--Delete the old current record from fuel_age
 					DELETE FROM fuel_age_test WHERE gid = cur_rec.gid;
 
-					INSERT INTO fuel_age_test SELECT cur_rec.gid,cur_rec.year1,ST_DIFFERENCE(a.geom,b.geom)
+					--Recreate the current record with the new geometry
+					INSERT INTO fuel_age_test VALUES(cur_rec.gid,cur_rec.year1,ST_GeometryFromText(cur_geom,4326));
+
+					--Set cur_geom to null
+					cur_geom = null;
+				ELSIF new.year1 < cur_rec.year1 THEN
+					--Get the geometry of the inserted record (current geometry - new geometry)
+					cur_geom := (SELECT ST_AsText(ST_MULTI(ST_DIFFERENCE(b.geom,a.geom)))
 						FROM fuel_age_test AS a, dec_fire_history_test AS b
-						WHERE a.gid = cur_rec.gid and b.gid = new.gid;
-				
-					/*
-					UPDATE fuel_age_test SET geom = (SELECT ST_MULTI(ST_UNION(a.geom,ST_DIFFERENCE(a.geom,b.geom)))
-						FROM dec_fire_history_test AS b, fuel_age_test AS a
-						WHERE a.gid = new.gid and b.gid = cur_rec.gid)
-						WHERE gid = cur_rec.gid;
-					*/
+						WHERE a.gid = cur_rec.gid and b.gid = new.gid);
+
+					--Delete the old inserted record from fuel_age
+					DELETE FROM fuel_age_test WHERE gid = new.gid;
+
+					--Recreate the inserted record with the new geometry
+					INSERT INTO fuel_age_test VALUES(new.gid,new.year1,ST_GeometryFromText(cur_geom,4326));
+
+					--Set cur_geom to null
+					cur_geom = null;
+				ELSE
+					--Get the geometry of the inserted record (current geometry + new geometry)
+					cur_geom := (SELECT ST_AsText(ST_MULTI(ST_UNION(b.geom,a.geom)))
+						FROM fuel_age_test AS a, dec_fire_history_test AS b
+						WHERE a.gid = cur_rec.gid and b.gid = new.gid);
+
+					--Delete the old inserted record from fuel_age
+					DELETE FROM fuel_age_test WHERE gid = new.gid;
+
+					--Recreate the inserted record with the new geometry
+					INSERT INTO fuel_age_test VALUES(new.gid,new.year1,ST_GeometryFromText(cur_geom,4326));
+
+					--Set cur_geom to null
+					cur_geom = null;
 				END IF;
 			END LOOP;
 		ELSE
@@ -37,10 +65,11 @@ BEGIN
 			FOR cur_rec IN SELECT * FROM fuel_age_test AS a JOIN dec_fire_history AS b ON ST_Intersects(a.geom, b.geom) WHERE b.gid = new.gid ORDER BY a.year1 LOOP
 				--If the inserted record is older than the intersecting record update the geometry of the intersecting record
 				IF new.year1 > cur_rec.year1 THEN
-					UPDATE fuel_age_test SET geom = (SELECT ST_MULTI(ST_UNION(a.geom,ST_DIFFERENCE(a.geom,b.geom)))
-						FROM dec_fire_history_test AS b, fuel_age_test AS a
-						WHERE a.gid = new.gid and b.gid = cur_rec.gid)
-						WHERE gid = cur_rec.gid;
+					--DELETE FROM fuel_age_test WHERE gid = cur_rec.gid;
+				
+					INSERT INTO fuel_age_test SELECT cur_rec.gid,cur_rec.year1,ST_MULTI(ST_DIFFERENCE(a.geom,b.geom))
+						FROM fuel_age_test AS a, dec_fire_history_test AS b
+						WHERE a.gid = cur_rec.gid and b.gid = new.gid;
 				END IF;
 			END LOOP;
 		END IF;
