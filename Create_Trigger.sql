3e9150d37036f2eb86e0a683a48ea2f6fc124c4f
Andrew Dennison <andrew.s.dennison@gmail.com>
2013-01-11 14:21:03 +0800

    Update Trigger

--- Create_Trigger.sql
+++ Create_Trigger.sql
@@ -1,48 +1,71 @@
+CREATE OR REPLACE FUNCTION calc_fuel_age() RETURNS trigger as $$
+DECLARE
+	cur_rec 	RECORD; --cursor variable
+	--fire_history 	-- name of fire history table
+	--fuel_age 	--name of fuel load table
+BEGIN
+	IF (TG_OP = 'INSERT') THEN
+		-- Check for intersecting polygons, if there aren't any just insert the record into fuel_history
+		IF EXISTS (SELECT * FROM fuel_age_test AS a JOIN dec_fire_history_test AS b ON ST_Intersects(a.geom, b.geom) WHERE b.gid = new.gid) THEN
+			--Insert the new record into fuel_history
+			INSERT INTO fuel_age_test SELECT gid,year1,geom FROM dec_fire_history_test WHERE gid = new.gid;
+			--Loop through the intersecting polygons
+			FOR cur_rec IN SELECT * FROM fuel_age_test AS a JOIN dec_fire_history_test AS b ON ST_Intersects(a.geom, b.geom) WHERE b.gid = new.gid ORDER BY a.year1 LOOP
+				--If the inserted record is older than the intersecting record update the geometry of the intersecting record
+				IF new.year1 > cur_rec.year1 THEN
+					DELETE FROM fuel_age_test WHERE gid = cur_rec.gid;
+
+					INSERT INTO fuel_age_test SELECT cur_rec.gid,cur_rec.year1,ST_DIFFERENCE(a.geom,b.geom)
+						FROM fuel_age_test AS a, dec_fire_history_test AS b
+						WHERE a.gid = cur_rec.gid and b.gid = new.gid;
+				
+					/*
+					UPDATE fuel_age_test SET geom = (SELECT ST_MULTI(ST_UNION(a.geom,ST_DIFFERENCE(a.geom,b.geom)))
+						FROM dec_fire_history_test AS b, fuel_age_test AS a
+						WHERE a.gid = new.gid and b.gid = cur_rec.gid)
+						WHERE gid = cur_rec.gid;
+					*/
+				END IF;
+			END LOOP;
+		ELSE
+			INSERT INTO fuel_age_test SELECT gid,year1,geom FROM dec_fire_history_test WHERE gid = new.gid;
+		END IF;
+	ELSIF (TG_OP = 'UPDATE') THEN
+		-- Check for intersecting polygons
+		IF EXISTS (SELECT * FROM fuel_age_test AS a JOIN dec_fire_history AS b ON ST_Intersects(a.geom, b.geom) WHERE b.gid = new.gid) THEN
+			--Loop through the intersecting polygons
+			FOR cur_rec IN SELECT * FROM fuel_age_test AS a JOIN dec_fire_history AS b ON ST_Intersects(a.geom, b.geom) WHERE b.gid = new.gid ORDER BY a.year1 LOOP
+				--If the inserted record is older than the intersecting record update the geometry of the intersecting record
+				IF new.year1 > cur_rec.year1 THEN
+					UPDATE fuel_age_test SET geom = (SELECT ST_MULTI(ST_UNION(a.geom,ST_DIFFERENCE(a.geom,b.geom)))
+						FROM dec_fire_history_test AS b, fuel_age_test AS a
+						WHERE a.gid = new.gid and b.gid = cur_rec.gid)
+						WHERE gid = cur_rec.gid;
+				END IF;
+			END LOOP;
+		END IF;
+	ELSIF (TG_OP = 'DELETE') THEN
+		-- Check for intersecting polygons (there should always be at least one though)
+		IF EXISTS (SELECT * FROM fuel_age_test AS a JOIN dec_fire_history AS b ON ST_Intersects(a.geom, b.geom) WHERE b.gid = new.gid) THEN
+			--Loop through the intersecting polygons
+			FOR cur_rec IN SELECT * FROM fuel_age_test AS a JOIN dec_fire_history AS b ON ST_Intersects(a.geom, b.geom) WHERE b.gid = old.gid ORDER BY a.year1 LOOP
+				--If the deleted record is older than the intersecting record update the geometry of the intersecting record, otherwise just delete the intersecting record
+				IF old.year1 > cur_rec.year1 THEN
+					UPDATE fuel_age_test SET geom = (SELECT ST_MULTI(ST_UNION(a.geom,ST_DIFFERENCE(a.geom,b.geom)))
+						FROM dec_fire_history_test AS b, fuel_age_test AS a
+						WHERE a.gid = old.gid and b.gid = cur_rec.gid)
+						WHERE gid = cur_rec.gid;
+				ELSE
+					DELETE FROM fuel_age_test WHERE gid = cur_rec.gid;
+				END IF;
+			END LOOP;
+		END IF;
+	END IF;
+	RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+--*****************************************************************************************************************
 CREATE TRIGGER update_test
 AFTER UPDATE OR INSERT OR DELETE ON dec_fire_history_test
 FOR EACH ROW
 EXECUTE PROCEDURE calc_fuel_age();
-
-CREATE OR REPLACE FUNCTION calc_fuel_age() RETURNS trigger as $update_test$
-DECLARE
-	id_array integer[];
-	cur_id integer;
-BEGIN
-	RAISE NOTICE 'Begin';
-	IF (TG_OP = 'DELETE') THEN
-		RAISE NOTICE 'Delete';
-		--Select fuel history records that intersect with the deleted fire history polygon
-		SELECT a.gid INTO id_array
-			FROM fuel_age_test AS a
-			JOIN dec_fire_history_test AS b
-			ON ST_Intersects(a.geom, b.geom)
-			WHERE b.gid = old.gid
-			ORDER BY a.gid;
-			
-		--Loop through intersecting polys
-		RAISE NOTICE 'Array Value',cur_id;
-		FOREACH cur_id IN ARRAY id_array 
-		LOOP
-			RAISE NOTICE 'Loop';
-			-- If the intersecting polygon is more recent than the deleted polygon clip it
-			IF row_data.year1 < old.year1 THEN
-				UPDATE fuel_age_test SET a.geom = (SELECT ST_DIFFERENCE(a.geom,b.geom)
-				FROM dec_fire_history_test AS b, fuel_age_test AS a
-				WHERE a.gid = row_data.gid and b.gid = old.gid)
-				WHERE gid = row_data.gid;
-			END IF;
-		END LOOP;
-	ELSIF (TG_OP = 'UPDATE') THEN
-		RAISE NOTICE 'Update';
-		--UPDATE fuel_age_test SET fuel_age_test.geom = NEW.geom FROM NEW WHERE fuel_age_test.gid = NEW.gid;
-		--DELETE FROM fuel_age_test WHERE fuel_age_test.gid = NEW.gid;
-		--INSERT INTO fuel_age_test SELECT * FROM dec_fire_history_test WHERE gid = NEW.gid;
-	ELSIF (TG_OP = 'INSERT') THEN
-		RAISE NOTICE 'Insert';
-		--INSERT INTO fuel_age_test SELECT * FROM dec_fire_history_test WHERE gid = NEW.gid;
-	ELSE
-		RAISE NOTICE 'Nothing Happened';
-	END IF;
-RETURN NEW;
-END;
-$update_test$ LANGUAGE plpgsql;
